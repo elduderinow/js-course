6455a571c520615bf9d7778c0c921202ef2def87
elduderinow <yarrut.franken@gmail.com>
2021-04-26 15:45:42 +0200

    update ex 3-3 dates

--- js-complete-course/03-dates/03-age-by-select/script.js
+++ js-complete-course/03-dates/03-age-by-select/script.js
@@ -9,22 +9,35 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
     let button = document.getElementById("run");
     button.addEventListener("click", DisplayInfo);
 
-
+    let cDate = new Date();
+    let cYear = cDate.getFullYear()
+    let cMon = cDate.getMonth() + 1;
+    let cDay = cDate.getDate();
 
     function DisplayInfo() {
         let day = document.getElementById("dob-day").value;
         let month = document.getElementById("dob-month").value;
         let year = document.getElementById("dob-year").value;
+        let age = cYear - year;
 
-        alert(`you were born on day ${day}, in the month ${month} in ${year}`);
+        if (cMon < month) {
+            age -= 1;
+
+        } else if (cMon == month && cDay < day) {
+
+            age -= 1;
+
+        }
+
+
+        alert(`You are ${age} years old.`);
+
+
     }
-
-
-
 
 
 })();
